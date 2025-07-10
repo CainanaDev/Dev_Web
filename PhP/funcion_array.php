f7348ffdcaf262d51fbbc5027a927c018943332c
CainanaDev <cainana10@gmail.com>
2025-07-10 15:44:53 -0300

    Praticas de funções nativas usadas em arrays. Nesse contudo temos as principais funções para manipular esses elementos, junto com uma pequena explicação e exemplo de como usa-los

---
+++ PhP/funcion_array.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Array</title>
+  </head>
+  <body>
+  <!--Os comentarios dentro das tags php, abaixo desse comentario, nao aparecem na pagina HTML gerada -->
+    <?php
+        /*
+        $array = array();
+          #a função is_array, verificar se uma variavel de fato é array
+          #e retorna true ou false
+        $retorno = is_array($array);
+        if ($retorno) {
+          echo 'Sim, é um array';
+        }else{
+          echo 'Não é um array';
+        }*/
+
+        /*
+        $array = [1 => 'a', 7=> 'b', 15 => 'c'];
+          echo '<pre>';
+            print_r($array);
+          echo '</pre>';
+          echo '<br/>';
+          #a função array_keys trás de volta as chaves do array em um novo array
+        $chaves_array = array_keys($array);
+        echo '<pre>';
+            print_r($chaves_array);
+          echo '</pre>';
+          echo '<br/>';
+        */
+        /*
+        $array = ['Mouse', 'Teclado', 'Fonte', 'Notebook', 'Gabinete'];
+        echo '<pre>';
+            print_r($array);
+          echo '</pre>';
+          echo '<br/>';
+            #a função sort organiza o array por ordem alfabetica, devolvendo true ou false, rearranjando os indices
+          #sort($array);
+          #echo '<pre>';
+          #  print_r($array);
+          #echo '</pre>';
+            #ja a função asort reordena por ordem alfabetica, mantendo os respectivos indices
+          asort($array);
+           echo '<pre>';
+            print_r($array);
+          echo '</pre>';
+          */
+
+          /*
+        $array = ['Mouse', 'Teclado', 'Fonte', 'Notebook', 'Gabinete'];
+        echo '<pre>';
+            print_r($array);
+          #função count conta a quantidade de elementos no array  
+          echo count($array);
+          echo '</pre>';
+          echo '<br/>';
+          */
+          $array1 =['Tv', 'DVD', 'Som'];
+          $array2 =['Sofa', 'Poltrona', 'Mesa', 'Abaju'] 
+    ?>
+
+     
+      
+      
+  </body>
+</html>
